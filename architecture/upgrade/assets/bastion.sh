3df5c9006f84ec301529b57603011a173b7f2a85
Tobias Derksen <tobias.derksen@codecentric.de>
2019-03-20 19:43:59 +0100

    Upgrade architecture

--- architecture/upgrade/assets/bastion.sh
+++ architecture/upgrade/assets/bastion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum -y update
-yum -y install centos-release-openshift-origin311 epel-release NetworkManager firewalld
+yum -y install centos-release-openshift-origin310 epel-release NetworkManager firewalld
 yum -y install origin-clients nfs-utils nano python2-passlib  httpd-tools patch java-1.8.0-openjdk-headless git gcc openssl-devel
 yum -y install python-passlib pyOpenSSL python-cryptography python-lxml ansible python2-pip python-devel
 systemctl enable firewalld NetworkManager
